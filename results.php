f1370faea9fba081a1e04186eabf20033daa626e
Tenzin Dhedon <59035997+tenzindhedon@users.noreply.github.com>
2020-10-11 11:46:28 -0300

    Update results.php

--- results.php
+++ results.php
@@ -13,46 +13,28 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // Get the quiz form from global session variable
             $quiz = $_SESSION['quiz'];
-            //var_dump($_SESSION['quiz']);
-        
-        // print the title of the result
             echo "<h1>Result:</h1>";
-            
-        // store the questions in an array 
             $questions = $quiz["questions"];
-           
-        // check if all questions are answered. Direct to error page otherwise
             for($i=0;$i<count($questions);$i++){
                 if ($_POST["answer$i"]==null) {
                     header("location:errorPage.php");
                 } 
             }
             
-        // store the quiz result in a variable
             $quizResult = "<div>";
-       
-        // run a loop through the question arry to display the questions and its anwser options
-            $count = 1; // for question numbering
-            $score = 0; // for counting score
+            $count = 1;
+            $score = 0; 
             
             for($i=0; $i<count($questions); $i++){
                 $quizResult .= "<div class='questionBox'>";
-                // question number
                 $quizResult .= "<h2>Question " .  $count++ . "</h2>"; 
-                // question text
                 $quizResult .= "<p>" . $questions[$i]['questionText'] . "</p>";
-                
-                // run a loop to print bulleted list of answers
-                    for($c=0; $c<count($questions[$i]["choices"]); $c++){
+                 for($c=0; $c<count($questions[$i]["choices"]); $c++){
                         $quizResult .= "<input type='radio' disabled>" . $questions[$i]["choices"][$c] . "<br>";
                     }
-                
-                // get the correct answer
                     $correctAnswer = $questions[$i]["choices"][$questions[$i]["answer"]];
                     
-                // check and print correct and incorrect answers
                     if($_POST["answer$i"] == $correctAnswer){
                         $quizResult .= "<p class='correct'>Your answer: " . $_POST["answer$i"] . " (correct)</p>";
                         $score++;
@@ -63,12 +45,10 @@ and open the template in the editor.
                 $quizResult .= "</div>";
             }
         
-        // print the score
             $quizResult .= "<div id='score'><h2>Score:</h2><p>Your score is <strong>" . $score;
             $quizResult .= "</strong> out of <strong>" . count($questions) . "</strong>.</p></div>";
             $quizResult .= "</div>";
-            
-        // print the result
+
             echo $quizResult;
         ?>
     </body>
