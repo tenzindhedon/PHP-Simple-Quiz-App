3f6706215664f7989be948110ce2887ca1ae0280
Tenzin Dhedon <59035997+tenzindhedon@users.noreply.github.com>
2020-10-11 11:47:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -16,35 +16,20 @@ and open the template in the editor.
         include 'ChromePhp.php';
         include 'FileUtils.php';
         
-        // get the json file
             $fileName = "WorldGeography.json";
             $fileContents = readFileIntoString($fileName);
             
-        // copy quiz to global session variable
             $_SESSION['quiz'] = json_decode($fileContents, true);
-        // convert the json file
-            $quiz = json_decode($fileContents, true); // true = array
-            
-        // print the title of the quiz
+            $quiz = json_decode($fileContents, true); 
             echo "<h1>" . $quiz["title"] . "</h1>";
-        
-        // store the questions in an array 
             $questions = $quiz["questions"];
-        
-        // store the form in a variable
             $quizForm = "<form action='results.php' method='post'>";
-       
-        // run a loop through the question arry to display the questions and its anwser options
-            $count = 1; // for question numbering
+            $count = 1; 
             
             for($i=0; $i<count($questions); $i++){
                 $quizForm .= "<div class='questionBox'>";
-                // question number
                 $quizForm .= "<h2>Question " .  $count++ . "</h2>"; 
-                // question text
                 $quizForm .= "<p>" . $questions[$i]['questionText'] . "</p>";
-                // run a loop to print bulleted list of answers
-                    
                     for($c=0; $c<count($questions[$i]["choices"]); $c++){
                         $quizForm .= "<input type='radio' name='answer$i' value='";
                         $quizForm .= $questions[$i]["choices"][$c] . "'>" . $questions[$i]["choices"][$c] . "<br>";
@@ -55,7 +40,6 @@ and open the template in the editor.
             $quizForm .= "<input id='submit' type='submit' value='Submit'>";
             $quizForm .= "</form>";
             
-        // print the form
             echo $quizForm;
         ?>
     </body>
